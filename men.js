e1955df797b570f6152ec4c797b3432012c95161
vladt89 <vladt89@yandex.ru>
2016-05-21 19:56:49 +0300

    Commit all

---
+++ men.js
@@ -0,0 +1,97 @@
+//<script language="JavaScript">
+//function mmLoadMenus() {
+//  if (window.mm_menu_0803092855_0) return;
+  window.mm_menu_0803092855_0 = new Menu("root",203,18,"Courier New, Courier, monospace",12,"#003333","#009966","#FCFAF9","#000099","center","middle",3,0,500,-5,7,true,true,true,0,true,true);
+  mm_menu_0803092855_0.addMenuItem("Cемиструнная&nbsp;гитара","location='guitar.html'");
+  mm_menu_0803092855_0.addMenuItem("Велосипед","location='bike.html'");
+  mm_menu_0803092855_0.addMenuItem("Коньки&nbsp;и&nbsp;ролики","location='roller.html'");
+  mm_menu_0803092855_0.addMenuItem("Настольный&nbsp;теннис","location='prof_tennis.html'");
+   mm_menu_0803092855_0.hideOnMouseOut=true;
+   mm_menu_0803092855_0.bgColor='#000099';
+   mm_menu_0803092855_0.menuBorder=1;
+   mm_menu_0803092855_0.menuLiteBgColor='#99FF00';
+   mm_menu_0803092855_0.menuBorderBgColor='#777777';
+
+    window.mm_menu_0803103406_0_1 = new Menu("2008",171,18,"Courier New, Courier, monospace",12,"#003333","#009966","#FCFAF9","#000099","center","middle",3,0,500,-5,7,true,true,true,0,true,true);
+    mm_menu_0803103406_0_1.addMenuItem("Одесса","location='Odeca.html'");
+    mm_menu_0803103406_0_1.addMenuItem("Поход&nbsp;по&nbsp;Крыму","location='pohod.html'");
+    mm_menu_0803103406_0_1.addMenuItem("Выборг","location='vyborg.html'");
+    mm_menu_0803103406_0_1.addMenuItem("Москва","location='moscow.html'");
+    mm_menu_0803103406_0_1.addMenuItem("День&nbsp;рожденья","location='dr19.html'");
+    mm_menu_0803103406_0_1.addMenuItem("Настольный&nbsp;теннис","location='tennis.html'");
+    mm_menu_0803103406_0_1.addMenuItem("База&nbsp;Голубое&nbsp;озеро","location='baza.html'");
+    mm_menu_0803103406_0_1.addMenuItem("Мисс&nbsp;ХимФака","location='miss.html'");
+    mm_menu_0803103406_0_1.addMenuItem("Каток","location='katok.html'");
+     mm_menu_0803103406_0_1.hideOnMouseOut=true;
+     mm_menu_0803103406_0_1.bgColor='#3300FF';
+     mm_menu_0803103406_0_1.menuBorder=1;
+     mm_menu_0803103406_0_1.menuLiteBgColor='#99FF00';
+     mm_menu_0803103406_0_1.menuBorderBgColor='#777777';
+	 
+    window.mm_menu_0803103406_0_2 = new Menu("2009",171,18,"Courier New, Courier, monospace",12,"#003333","#009966","#FCFAF9","#000099","center","middle",3,0,500,-5,7,true,true,true,0,true,true);
+    mm_menu_0803103406_0_2.addMenuItem("моя&nbsp;машина","location='my8.html'");
+    mm_menu_0803103406_0_2.addMenuItem("ЦПКиО","location='cpkuo.html'");
+    mm_menu_0803103406_0_2.addMenuItem("ЛитМуз","location='litmus.html'");
+    mm_menu_0803103406_0_2.addMenuItem("ДР Тани и Леши","location='dr23.html'");
+    mm_menu_0803103406_0_2.addMenuItem("Одесса","location='Odesa09.html'");
+    mm_menu_0803103406_0_2.addMenuItem("Summer School","location='cbu.html'");
+    mm_menu_0803103406_0_2.addMenuItem("Зелек - лыжи","location='zelek_09.html'");
+     mm_menu_0803103406_0_2.hideOnMouseOut=true;
+     mm_menu_0803103406_0_2.bgColor='#3300FF';
+     mm_menu_0803103406_0_2.menuBorder=1;
+     mm_menu_0803103406_0_2.menuLiteBgColor='#99FF00';
+     mm_menu_0803103406_0_2.menuBorderBgColor='#777777';
+
+    window.mm_menu_0803103406_0_3 = new Menu("2010",171,18,"Courier New, Courier, monospace",12,"#003333","#009966","#FCFAF9","#000099","center","middle",3,0,500,-5,7,true,true,true,0,true,true);
+    mm_menu_0803103406_0_3.addMenuItem("Winter&nbsp;School","location='cbu_petr.html'");
+    mm_menu_0803103406_0_3.addMenuItem("Военные&nbsp;Сборы","location='war.html'");
+    mm_menu_0803103406_0_3.addMenuItem("База&nbsp;Горизонт","location='gorizont.html'");
+    mm_menu_0803103406_0_3.addMenuItem("Эстония","location='Estonia.html'");
+     mm_menu_0803103406_0_3.hideOnMouseOut=true;
+     mm_menu_0803103406_0_3.bgColor='#3300FF';
+     mm_menu_0803103406_0_3.menuBorder=1;
+     mm_menu_0803103406_0_3.menuLiteBgColor='#99FF00';
+     mm_menu_0803103406_0_3.menuBorderBgColor='#777777';
+	 
+     window.mm_menu_0803103406_0_4 = new Menu("2011",171,18,"Courier New, Courier, monospace",12,"#003333","#009966","#FCFAF9","#000099","center","middle",3,0,500,-5,7,true,true,true,0,true,true);
+    mm_menu_0803103406_0_4.addMenuItem("England","location='England.html'");
+    mm_menu_0803103406_0_4.addMenuItem("Moscow","location='Moscow_11.html'");
+    mm_menu_0803103406_0_4.addMenuItem("Защита","location='diploma.html'");
+    mm_menu_0803103406_0_4.addMenuItem("Выпускной","location='final_party.html'");
+    mm_menu_0803103406_0_4.addMenuItem("Одесса","location='Odessa_11.html'");
+    mm_menu_0803103406_0_4.addMenuItem("Поход по Карпатам","location='Karpatian.html'");
+    mm_menu_0803103406_0_4.addMenuItem("Бельгия","location='Belgium.html'");
+     mm_menu_0803103406_0_4.hideOnMouseOut=true;
+     mm_menu_0803103406_0_4.bgColor='#3300FF';
+     mm_menu_0803103406_0_4.menuBorder=1;
+     mm_menu_0803103406_0_4.menuLiteBgColor='#99FF00';
+     mm_menu_0803103406_0_4.menuBorderBgColor='#777777';
+	 
+	 
+     window.mm_menu_0803103406_0_5 = new Menu("2012",171,18,"Courier New, Courier, monospace",12,"#003333","#009966","#FCFAF9","#000099","center","middle",3,0,500,-5,7,true,true,true,0,true,true);
+    	mm_menu_0803103406_0_5.addMenuItem("Рига","location='Riga.html'");
+    	mm_menu_0803103406_0_5.addMenuItem("Joensuu","location='Joensuu.html'");
+    	mm_menu_0803103406_0_5.addMenuItem("Switzerland","location='Swiz.html'");
+				mm_menu_0803103406_0_5.addMenuItem("Europe Tour","location='eurotour_2012.html'");
+				mm_menu_0803103406_0_5.addMenuItem("Lapland","location='lapland.html'");
+    mm_menu_0803103406_0_5.hideOnMouseOut=true;
+     mm_menu_0803103406_0_5.bgColor='#3300FF';
+     mm_menu_0803103406_0_5.menuBorder=1;
+     mm_menu_0803103406_0_5.menuLiteBgColor='#99FF00';
+     mm_menu_0803103406_0_5.menuBorderBgColor='#777777';
+	 
+  window.mm_menu_0803103406_0 = new Menu("root",60,18,"Courier New, Courier, monospace",12,"#003333","#009966","#FCFAF9","#000099","center","middle",3,0,500,-5,7,true,true,true,0,true,true);
+  mm_menu_0803103406_0.addMenuItem(mm_menu_0803103406_0_1);
+  mm_menu_0803103406_0.addMenuItem(mm_menu_0803103406_0_2);
+  mm_menu_0803103406_0.addMenuItem(mm_menu_0803103406_0_3);
+  mm_menu_0803103406_0.addMenuItem(mm_menu_0803103406_0_4);
+  mm_menu_0803103406_0.addMenuItem(mm_menu_0803103406_0_5);
+   mm_menu_0803103406_0.hideOnMouseOut=true;
+   mm_menu_0803103406_0.childMenuIcon="arrows.gif";
+   mm_menu_0803103406_0.bgColor='#3300FF';
+   mm_menu_0803103406_0.menuBorder=1;
+   mm_menu_0803103406_0.menuLiteBgColor='#99FF00';
+   mm_menu_0803103406_0.menuBorderBgColor='#777777';
+
+mm_menu_0803103406_0.writeMenus();
+//} // mmLoadMenus()
